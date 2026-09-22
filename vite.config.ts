2f537269954663d97bccd328efa6a35232655981
Jeroen Robben <jeroen@robben.io>
2026-09-22 18:15:16 +0200

    deploy to GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/temporal-logic-explorer/',
   plugins: [react()],
   test: { globals: true, environment: 'jsdom', setupFiles: ['./src/test-setup.ts'] },
 });
